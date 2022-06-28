010706b432754d8517e9f13fd8a8df4149c81235
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-06-28 21:14:32 +0200

    added herosection

---
+++ components/Hero.js
@@ -0,0 +1,20 @@
+const Hero = () => {
+  return (
+    <div className="hero grow">
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content grow text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Välkommen</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Logga in</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
