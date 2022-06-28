6da520ef1b8352c721f7440c3c2bbbf66d367769
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-06-28 21:14:21 +0200

    changed to sign in with email, pwd

--- components/Auth.js
+++ components/Auth.js
@@ -1,16 +1,20 @@
-import { useState } from 'react'
+import { useState, useId } from 'react'
+import Link from 'next/link'
 import { supabase } from '../utils/supabaseClient'
 
-export default function Auth() {
+export default function Login() {
   const [loading, setLoading] = useState(false)
   const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-  const handleLogin = async (email) => {
+  const id = useId()
+
+  const handleLogin = async (email, pwd) => {
     try {
       setLoading(true)
-      const { error } = await supabase.auth.signIn({ email })
+      const { error } = await supabase.auth.signIn({ email, password })
       if (error) throw error
-      alert('Check your email for the login link!')
+      alert('Inloggad')
     } catch (error) {
       alert(error.error_description || error.message)
     } finally {
@@ -19,24 +23,34 @@ export default function Auth() {
   }
 
   return (
-    <div className="card bg-base-200 max-w-sm shadow-lg">
+    <div className="card bg-base-200 max-w-md shadow-lg">
       <div className="card-body">
         <h1 className="text-center text-3xl font-bold mb-6 uppercase">
           Logga in
         </h1>
-        <p className=" font-light">
-          Logga in via en magisk länk. Ange din email nedan.
-        </p>
+        <p className=" font-light">Ange din email och lösenord.</p>
         <div className="form-control">
-          <label className="label">
+          <label htmlFor={id + '-email'} className="label">
             <span className="label-text">Email</span>
           </label>
           <input
+            id={id + '-email'}
             className="input"
             type="email"
             placeholder="Din email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor={id + '-pwd'} className="label">
+            <span className="label-text">Lösenord</span>
+          </label>
+          <input
+            id={id + '-pwd'}
+            className="input"
+            type="password"
+            placeholder="Ditt lösenord"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="form-control mt-6">
@@ -48,9 +62,13 @@ export default function Auth() {
             className="btn btn-primary"
             disabled={loading}
           >
-            <span>{loading ? 'Loading' : 'Skicka magisk länk'}</span>
+            <span>{loading ? 'Loading' : 'Logga in'}</span>
           </button>
         </div>
+        <p>Inget konto än?</p>
+        <Link href="/signup">
+          <a>Skapa konto</a>
+        </Link>
       </div>
     </div>
   )
